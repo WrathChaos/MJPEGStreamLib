87b38fe2d30b722136e5fb8c118528bb79506888
WrathChaos <wrathchaos@hotmail.com>
2017-08-24 13:58:58 +0300

    Create project via pod

---
+++ Example/Pods/Target Support Files/MJPEGStreamLib/MJPEGStreamLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MJPEGStreamLib : NSObject
+@end
+@implementation PodsDummy_MJPEGStreamLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-MJPEGStreamLib_Example/Pods-MJPEGStreamLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MJPEGStreamLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MJPEGStreamLib_Example
+@end
